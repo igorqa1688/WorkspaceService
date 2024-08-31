c4ff1e9f515de6c565cb97781a0ef544e3636400
leviafan14 <14leviafan88@gmail.com>
2024-09-01 00:00:41 +0500

    add requests

--- requests.py
+++ requests.py
@@ -10,7 +10,7 @@ def create_workspace(club_guid: str) -> str:
         stub = workspace_service_pb2_grpc.WorkspaceServiceStub(channel)
 
         request = workspace_service_pb2.CreateWorkspaceRequest(
-            club_guid=workspace_service_pb2.GUID(value=f"{club_guid}"))
+            club_guid=workspace_service_pb2.GUID(value=club_guid))
         try:
             response = stub.CreateWorkspace(request)
             return response
@@ -72,12 +72,54 @@ def get_workspace(workspace_guid: str, club_guid: str) -> str:
             return e
 
 
+def put_user_to_workspace(workspace_guid: str, club_guid: str, user_guid: str, user_role: str, user_workspace_description: str ) -> str:
+    with grpc.insecure_channel(server) as channel:
+        stub = workspace_service_pb2_grpc.WorkspaceServiceStub(channel)
+
+        request = workspace_service_pb2.PutUserToWorkspaceRequest(
+            #club_guid=workspace_service_pb2.GUID(value=club_guid),
+            user_guid=workspace_service_pb2.GUID(value=user_guid),
+            user_role=workspace_service_pb2.WorkspaceUserRoleMessage.Value(user_role),
+            user_workspace_description=user_workspace_description,
+            workspace_guid=workspace_service_pb2.GUID(value=workspace_guid)
+        )
+        try:
+            print(f"request put_user_to_workspace:\n{request}")
+            response = stub.PutUserToWorkspace(request)
+            return response
+        except Exception as e:
+            return f"error: {e}"
+
+
+
+def add_visible_player_to_user(player_guids: str, workspace_guid: str, club_guid: str, user_guid: str) -> str:
+    with grpc.insecure_channel(server) as channel:
+        stub = workspace_service_pb2_grpc.WorkspaceServiceStub(channel)
+        request = workspace_service_pb2.AddVisiblePlayersToUserRequest(
+            #club_guid=workspace_service_pb2.GUID(value=club_guid),
+            user_guid=workspace_service_pb2.GUID(value=user_guid),
+            player_guids=[player_guids],
+            workspace_guid=workspace_service_pb2.GUID(value=workspace_guid)
+        )
+        try:
+            print(f"request add_visible_player_to_user:\n{request}")
+            response = stub.AddVisiblePlayersToUser(request)
+            return response
+        except Exception as e:
+            return f"error: {e}"
+
+
 if __name__ == "__main__":
     club_guid = generate_guid()
-    create_workspace(club_guid)
+    user_guid = generate_guid()
+    player_guid = generate_guid()
+    user_workspace_description = "UserDescription"
+    user_role = "ROLE_OWNER"
+    print("\ncreate_workspace response:\n",create_workspace(club_guid))
     all_workspaces = get_all_workspaces().workspaces
-    print(all_workspaces)
+    print(all_workspaces[0])
     workspace_guid = all_workspaces[0].workspace_guid.value
     workspace_club_guid = all_workspaces[0].club_guid.value
-    print(f"workspace_guid: {workspace_guid}\nworkspace_club_guid: {workspace_club_guid}")
-    print(get_workspace_by_club_guid(workspace_club_guid))
+
+    print("response put_user_to_workspace:\n", put_user_to_workspace(workspace_guid, workspace_club_guid, user_guid, user_role, user_workspace_description))
+    #print("response add_visible_player_to_user\n", add_visible_player_to_user(player_guid, workspace_guid, workspace_club_guid, user_guid))
