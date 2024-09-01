3e96b585ec77644a4e599b86e99f52a7e5ea7004
leviafan14 <14leviafan88@gmail.com>
2024-09-02 00:00:26 +0500

    test_get_all_workspaces

--- test_workpace_service.py
+++ test_workpace_service.py
@@ -2,7 +2,7 @@ import pytest
 from requests import get_all_workspaces
 
 
-# GetAllWorkspaces
+# test GetAllWorkspaces
 def test_get_all_workspaces():
     response = get_all_workspaces()
     assert len(response.workspaces) > 0
